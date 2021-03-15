6ffcb07f2e381be4fc05e8c6a04ea75c2d854d7b
Kevin Dam <kevindam96@gmail.com>
2021-03-14 21:48:42 -0700

    remove unnecessary test, already covered in another test case

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,7 +5,6 @@ require '../lib/tic_tac_toe'
 TIC_TAC_TOE_X = 'X'
 TIC_TAC_TOE_O = 'O'
 
-# rubocop: disable Metrics/BlockLength
 describe BoardModel do
   describe '#make_move' do
     board_model = BoardModel.new
@@ -21,37 +20,7 @@ describe BoardModel do
       expect(board_model.spaces).to eql([1, 2, 3, 4, TIC_TAC_TOE_O, 6, TIC_TAC_TOE_X, 8, 9])
     end
   end
-
-  describe '#refresh_board' do
-    board_model = BoardModel.new
-    board_view = BoardView.new
-    it 'updates the board as moves are made' do
-      board_model.make_move(7)
-      board_view.refresh_board(board_model)
-      expect(board_view.to_s).to eql("      |     |     \n"\
-        "   1  |  2  |  3  \n"\
-        " _____|_____|_____\n"\
-        "      |     |     \n"\
-        "   4  |  5  |  6  \n"\
-        " _____|_____|_____\n"\
-        "      |     |     \n"\
-        "   #{TIC_TAC_TOE_X}  |  8  |  9  \n"\
-        '      |     |     ')
-      board_model.make_move(5)
-      board_view.refresh_board(board_model)
-      expect(board_view.to_s).to eql("      |     |     \n"\
-        "   1  |  2  |  3  \n"\
-        " _____|_____|_____\n"\
-        "      |     |     \n"\
-        "   4  |  #{TIC_TAC_TOE_O}  |  6  \n"\
-        " _____|_____|_____\n"\
-        "      |     |     \n"\
-        "   #{TIC_TAC_TOE_X}  |  8  |  9  \n"\
-        '      |     |     ')
-    end
-  end
 end
-# rubocop: enable Metrics/BlockLength
 
 # rubocop: disable Metrics/BlockLength
 describe BoardView do
@@ -74,7 +43,6 @@ describe BoardView do
 
     it 'renders the tic-tac-toe board when the player makes the first move, filling in space 7' do
       board_model.make_move(7)
-      board_view.refresh_board(board_model)
       expect { board_view.render(board_model) }.to output(
         "      |     |     \n"\
         "   1  |  2  |  3  \n"\
@@ -90,7 +58,6 @@ describe BoardView do
 
     it 'renders the tic-tac-toe board when the second player makes the second move, filling in space 5' do
       board_model.make_move(5)
-      board_view.refresh_board(board_model)
       expect { board_view.render(board_model) }.to output(
         "      |     |     \n"\
         "   1  |  2  |  3  \n"\
